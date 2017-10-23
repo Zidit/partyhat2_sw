327b2758bff06b45a8bb0c123a9fb9c830724b91
Zidit <mika.terhokoski@gmail.com>
2017-10-23 19:19:13 +0300

    Fix strncmp

--- src/stdlib.c
+++ src/stdlib.c
@@ -57,7 +57,13 @@ char* strcpy(char *dest, const char *src)
 
 int strncmp (const char* str1, const char* str2, size_t num)
 {
-	while(num-- && *str1 && *str2 && *(str1++) == *(str2++));
+	while(num-- && *str1 && *str2 && *(str1) == *(str2)) {
+		str1++;
+		str2++;
+	}
+	if(!*str1 || !*str2)
+		return 0;
+
 	return *str1 - *str2;
 }
 
