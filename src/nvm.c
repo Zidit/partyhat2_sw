cb8f962c22336044913d93cfc2a807b6878912b6
Zidit <mika.terhokoski@gmail.com>
2018-08-04 15:58:01 +0300

    Fixed some animations

--- src/nvm.c
+++ src/nvm.c
@@ -8,6 +8,7 @@
 #include <chip.h>
 #include <string.h>
 #include "atomic.h"
+
 #define LONG_STR(...) #__VA_ARGS__
 
 char nvm_buffer[8][1024] __attribute__((aligned(256), section(".text_Flash2"))) =
@@ -29,33 +30,38 @@ char nvm_buffer[8][1024] __attribute__((aligned(256), section(".text_Flash2"))) 
 "140 let b = b + 3\n"
 "150 update l\n"
 "160 sleep 20\n"
-"170 goto 50\n",
+"170 goto 50\n"
+,
 
 "10 peek 1000, l\n"
-"20 poke 0, (rnd % 255) / 3 + 128\n"
-"30 poke 1, (rnd % 255) / 3 + 128\n"
-"40 poke 2, (rnd % 255) / 3 + 128\n"
+"20 poke 0, (rnd % 255)\n"
+"30 poke 1, (rnd % 255)\n"
+"40 poke 2, (rnd % 255)\n"
+"45 for j = 0 to 20\n"
 "50 gosub 200\n"
 "100 update l\n"
 "105 sleep 10\n"
-"110 goto 50\n"
+"110 next j\n"
+"120 goto 20\n"
 "200 for k = 0 to l * 3\n"
 "210 peek (l*3 - k), a\n"
 "240 poke (l*3 - k + 3), a\n"
 "250 next k\n"
 "260 for i = 0 to 2\n"
 "270 peek i + 3, a\n"
-"280 let a = a - 7 + rnd % 15\n"
+"280 let a = a - 16 + rnd % 15\n"
 "285 poke i, a\n"
 "290 next i\n"
-"300 return\n",
+"300 return\n"
+,
 
 "10 peek 1000, l\n"
-"20 for a = 0 to l\n"
+"20 for a = 0 to l*3 \n"
 "30 poke a, rnd % 255\n"
 "40 next a\n"
 "50 update l\n"
-"70 goto 20\n",
+"70 goto 20\n"
+,
 
 "10 peek 1000, l\n"
 "20 let a = 0\n"
@@ -64,21 +70,25 @@ char nvm_buffer[8][1024] __attribute__((aligned(256), section(".text_Flash2"))) 
 "50 let d = a\n"
 "60 let e = b\n"
 "70 let f = c\n"
-"80 let a = rnd % 255\n"
-"90 let b = rnd % 255\n"
-"100 let c = rnd % 255\n"
-"110 for i = 0 to 100\n"
+"80 let a = (rnd % 7 + 1) * 32\n"
+"90 let b = (rnd % 7 + 1) * 32\n"
+"100 let c = (rnd % 7 + 1) * 32\n"
+"101 if a = b then c = 0\n"
+"102 if a = c then b = 0\n"
+"103 if b = c then a = 0\n"
+"110 for i = 1 to 100\n"
+"111 let r = (d * (100 - i) + a * i) / 100\n"
+"112 let g = (e * (100 - i) + b * i) / 100\n"
+"113 let b = (f * (100 - i) + c * i) / 100\n"
 "120 for j = 0 to l\n"
-"130 poke(j * 3 + 0), (a * (100 - i) + d * i) / 100\n"
-"140 poke(j * 3 + 1), (b * (100 - i) + e * i) / 100\n"
-"150 poke(j * 3 + 2), (c * (100 - i) + f * i) / 100\n"
+"130 poke(j * 3 + 0), g\n"
+"140 poke(j * 3 + 1), r\n"
+"150 poke(j * 3 + 2), b\n"
 "160 next j\n"
 "170 update l\n"
-"180 sleep 10\n"
 "190 next i\n"
-"200 goto 50\n",
-
-
+"200 goto 50\n"
+,
 
 "10 peek 1000, l\n"
 "20 let c = 0\n"
@@ -96,9 +106,8 @@ char nvm_buffer[8][1024] __attribute__((aligned(256), section(".text_Flash2"))) 
 "130 let a = a + 3\n"
 "140 if a > 270 then goto 30\n"
 "150 update l\n"
-"170 goto 120\n",
-
-
+"170 goto 120\n"
+,
 
 "10 peek 1000, l\n"
 "20 let r = 1\n"
@@ -114,11 +123,11 @@ char nvm_buffer[8][1024] __attribute__((aligned(256), section(".text_Flash2"))) 
 "120 for i = 0 to 2\n"
 "130 update l\n"
 "140 next i\n"
-"150 goto 50\n",
+"150 goto 50\n"
+,
 
-
-
-"10 goto 10",
+"10 goto 10"
+,
 
 //Knight rider
 /*
@@ -127,7 +136,7 @@ char nvm_buffer[8][1024] __attribute__((aligned(256), section(".text_Flash2"))) 
 "30 for i = 0 to s\n"
 "40 gosub 200\n"
 "50 poke i * 3 + 1, 255\n"
-"60 update l\n" 
+"60 update l\n"
 "70 sleep 50\n"
 "80 next i\n"
 "100 for j = 0 to s\n"
